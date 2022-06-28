12277c20eff6d1c6b6466dc4eb661504693813f9
Linebeck <realmanofthesea@gmail.com>
2022-06-27 21:35:08 -0400

    Taken off main thread

--- src/main/java/com/hyrulecraft/gitter/handlers/GitHandler.java
+++ src/main/java/com/hyrulecraft/gitter/handlers/GitHandler.java
@@ -1,11 +1,13 @@
 package com.hyrulecraft.gitter.handlers;
 
 import com.hyrulecraft.gitter.internal.Main;
+import org.bukkit.Bukkit;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class GitHandler {
 
@@ -19,25 +21,29 @@ public class GitHandler {
     }
 
     public boolean cloneRepo() {
-        try {
-            File gitFile = new File(this.file + File.separator + ".git");
-            if(gitFile.exists()) {
-                Git git = Git.open(gitFile);
-                if(git != null) {
-                    git.pull().call();
-                    git.close();
+        AtomicBoolean success = new AtomicBoolean(false);
+        Bukkit.getScheduler().runTaskAsynchronously(Main.getInstance(), () -> {
+            try {
+                File gitFile = new File(this.file + File.separator + ".git");
+                if(gitFile.exists()) {
+                    Git git = Git.open(gitFile);
+                    if(git != null) {
+                        git.pull().call();
+                        git.close();
+                    }
+                } else {
+                    Git.cloneRepository()
+                            .setURI(this.repo)
+                            .setDirectory(this.file)
+                            .call()
+                            .close();
                 }
-            } else {
-                Git.cloneRepository()
-                        .setURI(this.repo)
-                        .setDirectory(this.file)
-                        .call()
-                        .close();
+                success.set(true);
+            } catch (GitAPIException | IOException exception) {
+                exception.printStackTrace();
             }
-        } catch (GitAPIException | IOException exception) {
-            exception.printStackTrace();
-            return false;
-        }
-        return true;
+        });
+
+        return success.get();
     }
 }
